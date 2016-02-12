2a7d318f3e0b528b938e995767197134ac0dcce7
Nicholas Ivanecky <ivanecky844@hotmail.com>
2016-02-12 00:52:12 -0500

    Adding Apple Watch Project

---
+++ WeatherWatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WeatherWatch Extension
+//
+//  Created by Nick on 2016-02-12.
+//  Copyright © 2016 Nicholas Ivanecky. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
